cc55711656438003ce62d7cc0b86d0388105ba3b
Noah Rorabaugh <noahrorabaugh@Noahs-MacBook-Pro.local>
2019-09-23 17:39:43 -0400

    First solution

---
+++ script.js
@@ -0,0 +1,11 @@
+let v = ""
+let numberOfRows = 10;
+let numberOfColumns = 10;
+for (let i = 0; i <numberOfColumns; i++) {
+    for (let i = 0; i < numberOfRows; i++) {
+        v += "V"
+    }
+    v += "\n"
+}
+
+console.log(v)
